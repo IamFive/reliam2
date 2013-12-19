5c8a4722ceda108d5d2bb59acdd0365fa2670b9d
woo <iampurse@vip.qq.com>
2013-12-19 20:11:04 +0800

    small fix

--- reliam/celeryd.py
+++ reliam/celeryd.py
@@ -39,7 +39,7 @@ def init_celery():
     class ContextTask(TaskBase):
         abstract = True
         def __call__(self, *args, **kwargs):
-            with app.app_context()():
+            with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
             
     celery.Task = ContextTask
